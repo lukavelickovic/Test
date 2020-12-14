217a6d8cd2df81445251c1e0a0b1b406251cf230
Sentinel <luka.velickoviccopy@gmail.com>
2020-12-14 11:44:15 +0100

    Test

---
+++ zadatak1.js
@@ -0,0 +1,12 @@
+let price = 200;
+let retiree = true;
+for(day = 1; day <= 7; day++) {
+    if (retiree = true) let racun = cena - 25% console.log(racun)
+    else if(retiree = true && day < 7) let racun = cena - 20% console.log(racun)
+    else if( day = 3) console.log(price - 20);
+    else console.log(price);
+    
+    
+  
+    
+}
---
+++ zadatak3.js
@@ -0,0 +1,7 @@
+let n = 194
+let m = 3
+for(i = m; i <= n; i++){
+    let zbir = i + n;
+    let count = zbir / n;
+    console.log(zbir / count)
+}
